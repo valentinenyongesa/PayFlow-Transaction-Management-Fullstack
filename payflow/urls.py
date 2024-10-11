914e1243102e9dff503e9cb6a523e3bf9fd65a6d
valentinenyongesa <valentinenyongesa94@gmail.com>
2024-10-11 04:49:41 +0300

    Add home view to handle root URL and avoid 404 errors

--- payflow/urls.py
+++ payflow/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to PayFlow!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('transactions/', include('transactions.urls')),
+    path('', home),
 ]
