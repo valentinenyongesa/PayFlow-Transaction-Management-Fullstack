1e2688f639b4dbaf3323902872fcd3be2ccd6001
valentinenyongesa <valentinenyongesa94@gmail.com>
2024-10-13 06:04:54 +0300

    Add views to handle transaction operations in the transactions app

--- transactions/views.py
+++ transactions/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
-
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from .models import Transaction
+from .serializers import TransactionSerializer
 
+@api_view(['GET'])
 def transaction_list(request):
     transactions = Transaction.objects.all()
-    return render(request, 'transactions/transaction_list.html', {'transactions': transactions})
+    serializer = TransactionSerializer(transactions, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def create_transaction(request):
+    serializer = TransactionSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
