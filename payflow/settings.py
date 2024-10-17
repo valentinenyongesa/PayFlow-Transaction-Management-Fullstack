37029ea1a8c18d349c26a3caf4922b2cdef188a7
valentinenyongesa <valentinenyongesa94@gmail.com>
2024-10-18 01:38:55 +0300

    Add project description and instructions to README.md

--- payflow/settings.py
+++ payflow/settings.py
@@ -38,11 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'transactions',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -122,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
--- transactions/views.py
+++ transactions/views.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 from .models import Transaction
 from .serializers import TransactionSerializer
 
@@ -7,11 +8,12 @@ from .serializers import TransactionSerializer
 def transaction_list(request):
     transactions = Transaction.objects.all()
     serializer = TransactionSerializer(transactions, many=True)
-    return Response(serializer.data)
+    return Response(serializer.data, status=status.HTTP_200_OK)  # Return HTTP 200 for successful GET request
 
 @api_view(['POST'])
 def create_transaction(request):
     serializer = TransactionSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-    return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)  # Return HTTP 201 on success
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  # Return HTTP 400 on validation failure
