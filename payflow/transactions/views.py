434a607bfc1fb0f723b5c333b8ba76c64a3e70c9
valentinenyongesa <valentinenyongesa94@gmail.com>
2024-10-31 05:49:24 +0300

    Configuring changes to add the delete button

--- payflow/transactions/views.py
+++ payflow/transactions/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, get_object_or_404
+from django.http import HttpResponse
+from .models import Transaction
 
-# Create your views here.
+def delete_transaction(request, transaction_id):
+    transaction = get_object_or_404(Transaction, id=transaction_id)
+    transaction.delete()
+    return redirect('transaction_list')
